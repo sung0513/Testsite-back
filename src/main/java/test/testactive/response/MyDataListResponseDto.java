f8e2521ee61135240c618288ac56b9ed7b0f7dd5
Hyungjin Joo <hjjooace@naver.com>
2020-03-03 17:42:12 +0900

    MyDataResponseDto 추가

---
+++ src/main/java/test/testactive/response/MyDataListResponseDto.java
@@ -0,0 +1,23 @@
+package test.testactive.response;
+
+import lombok.Getter;
+import test.testactive.domain.user.User;
+
+@Getter
+public class MyDataListResponseDto {
+    private Long id;
+    private String name;
+    private String password;
+    private String email;
+    private String address;
+    private String picture;
+
+    public MyDataListResponseDto(User entity){
+        this.id = entity.getId();
+        this.name = entity.getName();
+        this.password = entity.getEmail();
+        this.email = entity.getEmail();
+        this.address = entity.getAddress();
+        this.picture = entity.getPicture();
+    }
+}
