2020be00ffbeeb287a79e3ebbce9289e2566f903
jeonhyeonu <tkawnd111@gmail.com>
2020-03-05 02:53:58 +0900

    1.Testpackakge -> ChecklistTest class: delivery.save error ; 120    2. Testpackage -> ChecklistTest class : checklist.save error ; 129

--- src/main/java/test/testactive/repository/CheckRepository.java
+++ src/main/java/test/testactive/repository/CheckRepository.java
@@ -1,7 +1,4 @@
 package test.testactive.repository;
 
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import test.testactive.domain.Checklist;
-
-public interface CheckRepository extends JpaRepository<Checklist,Long>{}
+public class CheckRepository {
+}
