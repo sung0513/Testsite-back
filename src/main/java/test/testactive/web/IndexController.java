f1b0f18222109c5af94d6b248e3e47262e3d2c9f
jeonhyeonu <tkawnd111@gmail.com>
2020-03-13 15:32:22 +0900

    hyeonwoo ;  make chairman.mustache and index.js
     IndexController 64line update

--- src/main/java/test/testactive/web/IndexController.java
+++ src/main/java/test/testactive/web/IndexController.java
@@ -4,21 +4,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import test.testactive.config.auth.LoginUser;
 import test.testactive.config.auth.dto.SessionUser;
-import test.testactive.domain.Order;
-import test.testactive.domain.Store;
-import test.testactive.domain.user.User;
 //import test.testactive.domain.user.UserService;
-import test.testactive.repository.OrderRepository;
-import test.testactive.request.StoreSaveRequestDto;
 import test.testactive.service.FoodService;
 import test.testactive.service.MemberService;
 import test.testactive.service.OrderService;
 import test.testactive.service.StoreService;
 
-import javax.mail.Session;
 import javax.servlet.http.HttpSession;
 
 @RequiredArgsConstructor
@@ -62,73 +54,18 @@ public class IndexController {
 
         return "mydata-page";
     }
-//
-//    @GetMapping("/barket") //결과를 전송
-//    public String barket(Model model) {
-//
-//        model.addAttribute("Food", foodService.b_findFoods()); //음식이름, 가격
-//        model.addAttribute("Store", storeService.b_findStores()); //가게이름,전화번호
-////        model.addAttribute("Order", orderService.b_findOrders()); //배달상태
-//        model.addAttribute("member",memberService.b_findMembers());
-//
-//        return "barket";
-//    }
 
-//
-//
-//    @GetMapping("/user/signup")
-//    public String dispSignup() {
-//        return "/signup";
-//    }
-//
-//
-//
-//    // 회원가입 처리
-//    @PostMapping("/user/signup")
-//    public String execSignup(UserDto userDto) {
-//        userService.joinUser(userDto);
-//
-//        return "redirect:/user/login";
-//    }
-//
-//    // 로그인 페이지
-//    @GetMapping("/user/login")
-//    public String dispLogin() {
-//        return "/login";
-//    }
-//
-//    // 로그인 결과 페이지
-//    @GetMapping("/user/login/result")
-//    public String dispLoginResult() {
-//        return "/loginSuccess";
-//    }
-//
-//    // 로그아웃 결과 페이지
-//    @GetMapping("/user/logout/result")
-//    public String dispLogout() {
-//        return "/logout";
-//    }
-//
-//    // 접근 거부 페이지
-//    @GetMapping("/user/denied")
-//    public String dispDenied() {
-//        return "/denied";
-//    }
-//
-//    // 내 정보 페이지
-//    @GetMapping("/user/info")
-//    public String dispMyInfo() {
-//        return "/myinfo";
-//    }
-//
-//    // 어드민 페이지
-//    @GetMapping("/admin")
-//    public String dispAdmin() {
-//        return "/admin";
-//    }
-//
+
+
     @GetMapping("posts/save")
     public String postsSave(){
         return "posts-save";
+    }
+
+
+
+    @GetMapping("posts/chairman")
+    public String postsChairman(){
+        return "chairman";
     }
 }
